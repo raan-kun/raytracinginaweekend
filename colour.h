7f888c3c4deab603a8ab5ca1664e90cf0f360d28
raan <ticktock40000@gmail.com>
2021-11-05 01:19:46 +0800

    implemented diffuse material

--- colour.h
+++ colour.h
@@ -5,16 +5,24 @@
 
 #include "vec3.h"
 
+#include <vector>
 #include <iostream>
 
-void write_colour(std::ostream& out, colour pixel_colour, int samples_per_pixel)
+void write_colour(std::vector<int>& pixels, colour pixel_colour, int samples_per_pixel)
 {
+	auto r = pixel_colour.x();
+	auto g = pixel_colour.y();
+	auto b = pixel_colour.z();
+
 	// vals will be >1 since we are sampling multiple times per pixel, so we must scale them back down to [0,1]
 	auto scale = 1.0 / samples_per_pixel;
-	
-	out << static_cast<int>(255.999 * scale * pixel_colour.x()) << ' '
-		<< static_cast<int>(255.999 * scale * pixel_colour.y()) << ' '
-		<< static_cast<int>(255.999 * scale * pixel_colour.z()) << std::endl;
+	r *= scale;
+	g *= scale;
+	b *= scale;
+
+	pixels.push_back(static_cast<int>(256 * clamp(r, 0.0, 0.999)));
+	pixels.push_back(static_cast<int>(256 * clamp(g, 0.0, 0.999)));
+	pixels.push_back(static_cast<int>(256 * clamp(b, 0.0, 0.999)));
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -5,18 +5,24 @@
 #include "camera.h"
 #include "hittable_list.h"
 
-#include <iostream>
+#include <vector>
+#include <chrono>
 #include <sstream>
 #include <fstream>
+#include <iostream>
 
-colour ray_colour(const ray& r, const hittable& world)
+colour ray_colour(const ray& r, const hittable& world, int depth)
 {
 	hit_record rec;
+
+	// stop bouncing if we've exceeded the ray bounce limit
+	if (depth <= 0)
+		return colour(0, 0, 0);
+
 	if (world.hit(r, 0, infinity, rec)) {
-		return 0.5 * (rec.normal + colour(1, 1, 1));
+		point3 target = rec.p + rec.normal + random_in_unit_sphere();
+		return 0.5 * ray_colour(ray(rec.p, target - rec.p), world, depth - 1);
 	}
-	//vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
-	//return 0.5*colour(N.x() + 1, N.y() + 1, N.z() + 1);
 
 	// scale direction to unit length (-1.0 < y < 1.0)
 	vec3 unit_direction = unit_vector(r.direction()); 
@@ -28,16 +34,17 @@ colour ray_colour(const ray& r, const hittable& world)
 
 int main()
 {
-	// image
+	// options
 	const auto aspect_ratio = 16.0 / 9.0;
-	const long long upscale_factor = 4;
 	const long long image_width = 400;
 	const long long image_height = static_cast<int>(image_width/aspect_ratio);
+	const long long upscale_factor = 1;
 	const long long samples_per_pixel = 100;
+	const int max_depth = 50;
 
-	// image io
-	std::ofstream file_out;
-	std::ostringstream pixels;
+	// image internal storage
+	std::vector<int> pixel_array;
+	pixel_array.reserve(3 * image_width * upscale_factor * image_height * upscale_factor);
 
 	// world
 	hittable_list world;
@@ -48,6 +55,7 @@ int main()
 	camera cam;
 
 	// render
+	auto tp1 = std::chrono::high_resolution_clock::now();
 	for (int j = image_height - 1; j >= 0; --j) {
 		std::stringstream line;
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
@@ -60,19 +68,44 @@ int main()
 				// make ray for this pixel
 				ray r = cam.get_ray(u, v);
 				// render ray
-				pix += ray_colour(r, world);
+				pix += ray_colour(r, world, max_depth);
 			}
-			for (int u = 0; u < upscale_factor; ++u)
-				write_colour(line, pix, samples_per_pixel);
+			write_colour(pixel_array, pix, samples_per_pixel);
 		}
-		for (int u = 0; u < upscale_factor; ++u)
-			pixels << line.str();
 	}
+	auto tp2 = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> time_render = tp2 - tp1;
 
+	// write to file
+	std::ostringstream pixel_string;
+	auto tp3 = std::chrono::high_resolution_clock::now();
+	for (long long j = 0; j < image_height; ++j) {
+		std::ostringstream line;
+		for (long long i = 0; i < image_width; ++i) {
+			for (int u = 0; u < upscale_factor; u++) {
+				line << pixel_array[image_width * j * 3 + i * 3] << ' ';
+				line << pixel_array[image_width * j * 3 + i * 3 + 1] << ' ';
+				line << pixel_array[image_width * j * 3 + i * 3 + 2] << '\n';
+			}
+		}
+		for(int u = 0; u < upscale_factor; u++)
+			pixel_string << line.str();
+	}
+	auto tp4 = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> time_string_convert = tp4 - tp3;
+
+	auto tp5 = std::chrono::high_resolution_clock::now();
+	std::ofstream file_out;
 	file_out.open("out.ppm");
 	file_out << "P3\n" << image_width*upscale_factor << ' ' << image_height*upscale_factor << "\n255" << std::endl;
-	file_out << pixels.str();
-	std::cerr << "\nDone" << std::endl;
+	file_out << pixel_string.str();
+	std::cerr << "\nDone" << "\n\n" << std::endl;
 	file_out.close();
+	auto tp6 = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> time_file_write = tp6 - tp5;
+
+	std::cerr << "Render time: " << time_render.count() << 's' << std::endl;
+	std::cerr << "String conversion time: " << time_string_convert.count() << 's' << std::endl;
+	std::cerr << "File write time: " << time_file_write.count() << 's' << std::endl;
 	return 0;
 }
