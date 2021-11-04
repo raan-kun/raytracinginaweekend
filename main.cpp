18e2980e9e63351a657adb3d79f5e4f271b92b6a
raan <ticktock40000@gmail.com>
2021-11-05 01:30:06 +0800

    fix shadow acne

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ colour ray_colour(const ray& r, const hittable& world, int depth)
 	if (depth <= 0)
 		return colour(0, 0, 0);
 
-	if (world.hit(r, 0, infinity, rec)) {
+	if (world.hit(r, 0.001, infinity, rec)) {
 		point3 target = rec.p + rec.normal + random_in_unit_sphere();
 		return 0.5 * ray_colour(ray(rec.p, target - rec.p), world, depth - 1);
 	}
